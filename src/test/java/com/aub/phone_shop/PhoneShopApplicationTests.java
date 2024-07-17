b822be1b74519e80667b1f2da41402d13b862c62
Chhotrakunkhunphovodon <chukizz904@gmail.com>
2024-07-17 08:55:41 +0700

    My second commit

---
+++ src/test/java/com/aub/phone_shop/PhoneShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aub.phone_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
